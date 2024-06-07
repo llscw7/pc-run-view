f57d5893733023c10b1d825766beb479a1830480
llscw7 <56208949+lqt0327@users.noreply.github.com>
2024-06-07 22:00:28 +0800

    chore: test

--- src/modules/Home.vue
+++ src/modules/Home.vue
@@ -8,6 +8,22 @@ import OneLineList from './application/one-line-list/index.vue'
 import { useCardStore, useOptionStore } from '../store/store'
 import { useRouter } from 'vue-router'
 import { storeToRefs } from 'pinia'
+import {
+  Document,
+  Menu as IconMenu,
+  Location,
+  Setting,
+} from '@element-plus/icons-vue'
+
+
+const isCollapse = ref(false)
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+
 
 const router = useRouter()
 
@@ -118,15 +134,48 @@ const changeMenu = () => {
 <template>
   <div class="home">
     <div class="nav">
-      <span class="icon-menu" @click="changeMenu"></span>
-      <div class="nav-container" :class="{'actived': menuFlag}">
-        <ul class="list">
-          <li class="list-wrap">
-            <div class="list-icon"></div>
-            <div class="list-content" v-if="menuFlag">测试数据</div>
-          </li>
-        </ul>
-      </div>
+      <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
+        <el-radio-button :value="false">expand</el-radio-button>
+        <el-radio-button :value="true">collapse</el-radio-button>
+      </el-radio-group>
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        :collapse="isCollapse"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <el-sub-menu index="1">
+          <template #title>
+            <el-icon><location /></el-icon>
+            <span>Navigator One</span>
+          </template>
+          <el-menu-item-group>
+            <template #title><span>Group One</span></template>
+            <el-menu-item index="1-1">item one</el-menu-item>
+            <el-menu-item index="1-2">item two</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="Group Two">
+            <el-menu-item index="1-3">item three</el-menu-item>
+          </el-menu-item-group>
+          <el-sub-menu index="1-4">
+            <template #title><span>item four</span></template>
+            <el-menu-item index="1-4-1">item one</el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu>
+        <el-menu-item index="2">
+          <el-icon><icon-menu /></el-icon>
+          <template #title>Navigator Two</template>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <el-icon><document /></el-icon>
+          <template #title>Navigator Three</template>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <el-icon><setting /></el-icon>
+          <template #title>Navigator Four</template>
+        </el-menu-item>
+      </el-menu>
     </div>
     <Header ref="headerRef" :setCurrentListData="setCurrentListData" @setSearchListData="setSearchListData"></Header>
     <MoreLineList 
@@ -187,9 +236,17 @@ const changeMenu = () => {
       width: 30px;
       height: 30px;
       background-size: contain;
+      flex-shrink: 0;
     }
     .list-content {
     }
   }
+}
+</style>
+
+<style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
 }
 </style>
